5668cb45b800c91b3ec886fb17935cd04c11b209
antonylima <asc.lima15@gmail.com>
2026-02-09 11:19:13 -0300

    langchain

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print ("hello!!!")
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+import os, rich
